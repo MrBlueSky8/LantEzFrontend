613553bac349db0ecfe9f079d7c77c9dc5609e62
MrBlueSky <116617108+MrBlueSky8@users.noreply.github.com>
2025-04-05 23:40:40 -0500

    rutas y adición de rama de componentes del admin y del evaluador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { segGuard } from './guard/seguridad.guard';
+import { AdminComponent } from './components/admin/admin.component';
+import { EvaluadorComponent } from './components/evaluador/evaluador.component';
 
 export const routes: Routes = [
   {
@@ -18,4 +20,14 @@ export const routes: Routes = [
     component: HomeComponent,
     canActivate: [segGuard], // solo construcciones, se debe agregar a cada uno
   },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [segGuard], // solo construcciones, se debe agregar a cada uno
+  },
+  {
+    path: 'evaluador',
+    component: EvaluadorComponent,
+    canActivate: [segGuard], // solo construcciones, se debe agregar a cada uno
+  }
 ];
