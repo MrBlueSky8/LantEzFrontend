b2a90e28d9ee2259952fef6253c88882555993d0
MrBlueSky <116617108+MrBlueSky8@users.noreply.github.com>
2025-05-21 18:34:04 -0500

    preparacion de service de usuarios para listar usuarios

---
+++ src/app/models/usuariosLight.ts
@@ -0,0 +1,23 @@
+import { Empresas } from "./empresas";
+import { Roles } from "./roles";
+import { TipoDocumento } from "./tipo-documento";
+
+export class UsuariosLight {
+    id: number = 0;
+    email: string = '';
+    primer_nombre: string = '';
+    segundo_nombre: string = '';
+    apellido_p: string = '';
+    apellido_m: string = '';
+    genero: string = '';
+    prefijo_telefono: string = '';
+    telefono: string = '';
+    fecha_registro: Date= new Date(Date.now());
+    tipoDocumento: TipoDocumento = new TipoDocumento();
+    numero_doc: string = '';
+    fechanacimiento: Date = new Date(Date.now());
+    roles: Roles = new Roles();
+    empresas: Empresas = new Empresas();
+    estado: boolean = false;
+  }
+  
